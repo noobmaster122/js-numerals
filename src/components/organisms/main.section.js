81c1420490b7d71923b61b68fa176ce367590a83
noobmaster122 <noobmaster122@github.com>
2021-09-28 14:24:56 +0100

    fixing leading zero bug and empty string bug

--- src/components/organisms/main.section.js
+++ src/components/organisms/main.section.js
@@ -24,6 +24,7 @@ export const PuzzleSection = () => {
     }
 
     const clickHandler = () => {
+       
         if(!!!numericValue) return setStringValue("must enter a number !")
         if(!!!noLeadingZero(numericValue)) return setStringValue("cant accept value with leading zeros");
         if(!!!isNumber(numericValue)) return setStringValue("enter whole numbers only !");
